28a49e1c6ea2f419b15ef41f3aed5d1a31ecae88
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-27 22:22:21 -0500

    db and editor.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,47 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("JATE", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("JATE")) {
+        console.log("JATE database already exisits");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("JATE", { keyPath: "id", autoIncrement: true });
+      console.log("JATE database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const postDb = async (name, home, cell, email) => {
+  console.log("Post to the ase");
+  const JATEDb = await openDB("JATE", 1);
+  const tx = JATEDb.transaction("JATE", "readwrite");
+  const store = tx.objectStore("JATE");
+  const request = store.add({ name, home, cell, email });
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("GET all from the database");
+  const JATEDb = await openDB("JATE", 1);
+  const tx = JATEDb.transaction("JATE", "readonly");
+  const store = tx.objectStore("JATE");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
+
+export const deleteDb = async (id) => {
+  console.log("DELETE from the database", id);
+  const JATEDb = await openDB("JATE", 1);
+  const tx = JATEDb.transaction("JATE", "readwrite");
+  const store = tx.objectStore("JATE");
+  const request = store.delete(id);
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
